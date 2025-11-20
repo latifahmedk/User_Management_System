d40b1e3d80765765c242c08cd325f94802633165
latifahmedk <latif.bst18@gmail.com>
2025-11-20 23:03:16 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [
-      "https://usermanagment-1.onrender.com",
-      "http://localhost:5173"
-    ],
+    origin: 
+      "http://localhost:5173",
                 methods: ["GET", "POST", "PUT", "DELETE"],
         credentials:true
     })
